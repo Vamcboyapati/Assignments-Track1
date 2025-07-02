7e949d9f0895596216032c88bc4d5f619d161851
Boyapati vamsi <vamcboyapati246@gmail.com>
2025-07-02 17:37:28 +0530

    2ndjuly

---
+++ 2nd-july-ssignment.py
@@ -0,0 +1,61 @@
+# 1 What are variables and why are they called variables
+#A variable is typically described as a name that refers to a specific block of memory and they are called "variables" because the value they store can be changed or varied in incoming lines of code.
+ 
+#Assign two numbers to variables `a` and `b`, and print their sum.
+a=18
+b=7
+print(a+b)
+
+# 2.Write a Python program to subtract two numbers and print the result
+18
+7
+print(18-7)
+
+# 3.Multiply two variables and store the result in a third variable. Print all three.
+a=18
+b=7
+c=a*b
+print(a,b,c)
+
+# 4.Divide 10 by 3 and print the result with and without decimals
+print(10/3)
+print(10//3)
+
+# Use floor division `//` to divide 17 by 4. Print the output.
+print(17//4)
+
+# 6. Use the modulo operator `%` to check the remainder when 25 is divided by 6.
+print(25%6)
+
+# 7. Calculate and print the square of a number stored in a variable.
+a=18
+print(a**2)
+
+# 8. Assign values to three variables `x`, `y`, `z` and compute the average.
+x,y,z=18,7,45
+average=x+y+z/3
+print(average)
+
+# 9. Take a number and find its cube using the `**` operator.
+print(18**3)
+
+# 10. Create two variables `length` and `width`. Calculate and print area of a rectangle.
+length=18
+width=7
+area_of_a_rectangle=length*width
+print(area_of_a_rectangle)
+
+# 11. Assign a variable `total_marks = 450` and `obtained_marks = 375`. Find percentage.
+total_marks=450
+obtained_marks=375
+percentage=(obtained_marks/total_marks)*100
+print(percentage)
+
+# 12. Write a Python statement that calculates `(a + b) * c` for some values of a, b, and c.
+a=18
+b=7
+c=45
+print((a+b)*c)
+
+# 13. Draw and show how reassignment changes variable reference to a memory block.
+
