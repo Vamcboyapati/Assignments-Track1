871a978d1e7d943239492b37acbd996459eff584
Boyapati vamsi <vamcboyapati246@gmail.com>
2025-07-03 11:50:35 +0530

    2ndjuly

--- 2ndjuly/2nd-july-assignment.py
+++ 2ndjuly/2nd-july-assignment.py
@@ -6,9 +6,13 @@ a=18
 b=7
 print(a+b)
 
+#output:25
+
 # 2.Write a Python program to subtract two numbers and print the result
 result=18-7
 print(result)
+
+#output:11
 
 # 3.Multiply two variables and store the result in a third variable. Print all three.
 a=18
@@ -16,27 +20,42 @@ b=7
 c=a*b
 print(a,b,c)
 
+#output:18 7 126
+
 # 4.Divide 10 by 3 and print the result with and without decimals
 print(10/3)
 print(10//3)
 
+#output:3.3333333333333335
+#output:3
+
 # Use floor division `//` to divide 17 by 4. Print the output.
 print(17//4)
+
+#output:4
 
 # 6. Use the modulo operator `%` to check the remainder when 25 is divided by 6.
 print(25%6)
 
+#output:1
+
 # 7. Calculate and print the square of a number stored in a variable.
 a=18
 print(a**2)
+
+#output:324
 
 # 8. Assign values to three variables `x`, `y`, `z` and compute the average.
 x,y,z=18,7,45
 average=(x+y+z)/3
 print(average)
 
+#output:23.333333333333332
+
 # 9. Take a number and find its cube using the `**` operator.
 print(18**3)
+
+#output:5832
 
 # 10. Create two variables `length` and `width`. Calculate and print area of a rectangle.
 length=18
@@ -44,17 +63,23 @@ width=7
 area_of_a_rectangle=length*width
 print(area_of_a_rectangle)
 
+#output:126
+
 # 11. Assign a variable `total_marks = 450` and `obtained_marks = 375`. Find percentage.
 total_marks=450
 obtained_marks=375
 percentage=(obtained_marks/total_marks)*100
 print(percentage)
 
+#output:83.33333333333334
+
 # 12. Write a Python statement that calculates `(a + b) * c` for some values of a, b, and c.
 a=18
 b=7
 c=45
 print((a+b)*c)
+
+#output:1125
 
 # 13. Draw and show how reassignment changes variable reference to a memory block.
 
